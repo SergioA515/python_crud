de9b87a45d228291341ccda370364c62d3c27fa2
Sergio Alejandro <110745388+SergioA515@users.noreply.github.com>
2023-03-07 05:13:33 -0500

    CRUD python

--- app.py
+++ app.py
@@ -2,24 +2,28 @@
     - CRUD basico con Flask
 """
 #Importación de paquetes necesarios que nos brinda Flask para su entorno Web
-from flask import Flask, render_template, request
-#import config.db_config as db
+from flask import Flask, render_template, request, redirect, url_for, flash
+from db_config import mysql,app
 from flask_mysqldb import MySQL
 from flask import Flask
-app = Flask(__name__)
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'password'
-app.config['MYSQL_DB'] = 'crud_example'
-mysql = MySQL(app)
 #Se inicializa la estructira y se definen los modulos necesarios ya importados
-app = Flask(__name__)
-mysql = MySQL(app)
 
-#comenzamos a defiir las rutas de nuestra aplicación web
+
+# conexión de mysql 
+# app.config['MYSQL_HOST'] = 'localhost'
+# app.config['MYSQL_USER'] = 'root'
+# app.config['MYSQL_PASSWORD'] = ''
+# app.config['MYSQL_DB'] = 'crud_example'
+# mysql = MySQL(app)
+# llave secreta que ayuda a compartir datos en una sesion
+
+#comenzamos a definir las rutas de nuestra aplicación web
 @app.route('/')
-def main_index():
-    return render_template('index.html')
+def Main_index():
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM user')
+    data = cur.fetchall()
+    return render_template('index.html', users = data)
 
 @app.route('/createUser', methods=['POST'])
 def create_user():
@@ -31,15 +35,34 @@ def create_user():
         cur = mysql.connection.cursor()
         cur.execute("INSERT INTO user (fullname_user, phone_user, email_user, password_user) VALUES (%s, %s, %s, %s)",(fullname, phone, email, password))
         mysql.connection.commit()
-        return 'Done'
+        flash('Success') 
+        return redirect(url_for('Main_index'))
 
-@app.route('/getUser')
-def get_user():
-    return "<h1>Formulario para Obtener un Usuario</h1>"
+@app.route('/updateUser/<id>', methods=['POST'])
+def update_user(id):
+    if request.method == 'POST':
+        fullname = request.form['fullname']
+        phone = request.form['phone']
+        email = request.form['email']
+        password = request.form['password']
+        cur = mysql.connection.cursor()
+        cur.execute("""
+            UPDATE user
+            SET fullname_user = %s, phone_user = %s, email_user = %s, password_user = %s
+            WHERE id_user = %s
+        """, (fullname, phone, email, password, id))
+        flash('Contacto Actualizado')
+        mysql.connection.commit()
+        return redirect(url_for('app.Main_index'))
 
-@app.route('/updateUser')
-def update_user():
-    return "<h1>Formulario para Actualizar un Usuario</h1>"
+@app.route('/getUser/<id>', methods=['POST', 'GET'])
+def get_user(id):
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM user WHERE id_user = %s', (id))
+    data = cur.fetchall()
+    cur.close()
+    print(data[0])
+    return render_template('edicion_usuario.html', users = data[0])
 
 @app.route('/deleteUser')
 def delete_user():
---
+++ config/crud_example.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 07-03-2023 a las 11:11:28
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `crud_example`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `fullname_user` varchar(100) NOT NULL,
+  `phone_user` varchar(255) NOT NULL,
+  `email_user` varchar(255) NOT NULL,
+  `password_user` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `user`
+--
+
+INSERT INTO `user` (`id_user`, `fullname_user`, `phone_user`, `email_user`, `password_user`) VALUES
+(1, 'Sergio Alejandro Parra Toro', '1234567890', 'sergio@mail.com', 'sergio12345');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
